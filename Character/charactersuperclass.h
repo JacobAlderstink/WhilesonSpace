e0d5500f3654db843812a67eb9c7d443b13c1e3e
Trevor <grif1718@vandals.uidaho.edu>
2018-04-16 14:45:36 -0700

    I'll admit I'm a little confused

--- Character/charactersuperclass.h
+++ Character/charactersuperclass.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef CHARACTER_H
 #define CHARACTER_H
 #include <QGraphicsRectItem>
@@ -27,33 +26,5 @@ protected:
 };
 
 #endif // CHARACTER_H
-=======
-#ifndef CHARACTER_H
-#define CHARACTER_H
-#include <QGraphicsRectItem>
-#include <QObject>
-#include "game.h"
-class Game;
-enum facing {leftDir,rightDir};
-class CharacterSuperClass : public QObject, public QGraphicsRectItem
-{
-    Q_OBJECT
-private:
 
 
-public:
-    Character();
-    double GetMass(){
-        return mass;    //should affect terminal velocity
-    }
-
-protected:
-    facing WhichWay;
-    double mass;
-    double velocityHor;
-
-
-};
-
-#endif // CHARACTER_H
->>>>>>> 64a9c1cfecda8c0f3da5323b947fc24fae299f62
