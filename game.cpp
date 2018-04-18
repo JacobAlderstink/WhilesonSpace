e7bed79f08a96f183920e101eb6df51c6c65627a
JacobAlderstink <38391791+JacobAlderstink@users.noreply.github.com>
2018-04-18 14:54:01 -0700

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -9,21 +9,22 @@ Game::Game()
  */
     //creating the scene
     scene = new QGraphicsScene();
-    scene->setSceneRect(0,0,1500,1500);
+
+    scene->setSceneRect(0,0,1920,1080);
 
     //adding scene to view
     setScene(scene);
 
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff); // this will have to be turned off to allow character tracking
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setFixedSize(1500,1500);                              // Will need to be changed to setFixedHeight(HEIGHT)
+    //setFixedSize(1920,1500);                              // Will need to be changed to setFixedHeight(HEIGHT)
 
     //Just here for gravity testing purposes
   /*  CharacterSuperClass *Character = new CharacterSuperClass();
     Character->setPos(200,200);
     scene->addItem(Character);
 */
-    show();
+    showFullScreen();
 
 }
 
