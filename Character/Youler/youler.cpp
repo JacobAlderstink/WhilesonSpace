59b0dccdc15595272ec370b68ee9dcbae08398a7
Trevor <grif1718@vandals.uidaho.edu>
2018-04-20 13:00:27 -0700

    Good Luck

--- Character/Youler/youler.cpp
+++ Character/Youler/youler.cpp
@@ -10,7 +10,7 @@ Youler::Youler(Game *Chargame): CharacterSuperClass(Chargame)
     CharWidth = 2/45;
     CharHeight = 4/39;
 
-
+    setRect( 0, 0, Chargame->getScene()->width()*CharWidth, Chargame->getScene()->height()*CharHeight );
 
     connect(Chargame->TimerGravity,SIGNAL(timeout()),this,SLOT(MoveUp()));
     connect(Chargame->TimerGravity,SIGNAL(timeout()),this,SLOT(MoveDown()));
