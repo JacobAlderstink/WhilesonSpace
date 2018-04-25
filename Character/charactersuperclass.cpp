ed169446a16c700b58b2fb30657547effefc9f64
JacobAlderstink <38391791+JacobAlderstink@users.noreply.github.com>
2018-04-25 14:53:39 -0700

    Add files via upload

--- Character/charactersuperclass.cpp
+++ Character/charactersuperclass.cpp
@@ -1,6 +1,6 @@
 #include "charactersuperclass.h"
 #include <QDebug>
-#include "chunckblock.h"
+
 void CharacterSuperClass::Gravity()
 {
     if(GravityOn == true)
@@ -19,17 +19,6 @@ void CharacterSuperClass::Gravity()
 
 void CharacterSuperClass::MoveRight()
 {
-    GravityOn=true;
-    QList<QGraphicsItem *> colliding_items = collidingItems(/*Q_OBJECT*/);
-       for(int i = 0, n = colliding_items.size(); i < n; ++i)
-       {
-            if(typeid(*(colliding_items[i]))==typeid(ChunckBlock))
-            {
-                GravityOn = false;
-            }
-
-       }
-
        if(MoveR == true)
         setPos(x()+velocityHor,y());
 }
